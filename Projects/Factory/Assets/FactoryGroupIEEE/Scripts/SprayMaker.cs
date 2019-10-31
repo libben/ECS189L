a2b411e2c56f94e52b2d42b4e537a3b1dff780c7
Vincent Le <vvvle@ucdavis.edu>
2019-10-31 14:35:09 -0700

    Added duration field for Spray spell, edited SpellFactory's build spray case

--- Projects/Factory/Assets/FactoryGroupIEEE/Scripts/SprayMaker.cs
+++ Projects/Factory/Assets/FactoryGroupIEEE/Scripts/SprayMaker.cs
@@ -11,34 +11,31 @@ namespace IEEE
 		private GameObject prefab;
 		[SerializeField]
 		private int ProjectileCount = 12;
+		[SerializeField]
+		private float Duration = 7.5f;
 		private float VelocityStep;
-
+		
 		void Start()
 		{
 			if (ProjectileCount == 1)
 				VelocityStep = 0.0f;
 			else
 				VelocityStep = 2.0f / (ProjectileCount - 1);
-			//Debug.Log(VelocityStep.ToString());
 		}
 
 		public GameObject Make()
 		{
 			GameObject newObject;
-
-			/*
-			newObject = Instantiate(this.prefab, this.gameObject.transform);
-			var firstObjectMotion = newObject.GetComponent<SprayMotion>();
-			firstObjectMotion.SetZSpeed(VelocityStep, 0);
-			Destroy(newObject, 5.0f);
-			*/
+			
 			for (int i = 0; i < ProjectileCount; i++)
 			{
 				newObject = Instantiate(this.prefab, this.gameObject.transform);
 				var objectMotion = newObject.GetComponent<SprayMotion>();
 				objectMotion.SetZSpeed(VelocityStep, i);
-				Destroy(newObject, 5.0f);
+				Destroy(newObject, Duration);
 			}
+
+			// Since many bullets were created, I'll just have it return any bullet.
 			newObject = GameObject.Find("SprayBullet");
 			return newObject;
 		}
