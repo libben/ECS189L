a4108abe20c4edb154c0d9621ebf620ef15c9637
Ben Rausch <slimeguy46@gmail.com>
2019-10-31 13:28:11 -0700

    motion

--- Projects/Factory/Assets/FactoryGroupIEEE/Scripts/BoomerangMotion.cs
+++ Projects/Factory/Assets/FactoryGroupIEEE/Scripts/BoomerangMotion.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 namespace IEEE {
 public class BoomerangMotion : MonoBehaviour
 {
+    private const float MaxForwardDistance = 30f;
+    private const float MaxSidewaysDisplacement = 10f;
+    private const float JourneyDuration = 4f;
+    private float TimeElapsedInJourney = 0f;
     private static Vector3 YAxis = new Vector3(0, 1, 0);
     float rotationSpeedInRadians = 2*Mathf.PI;
     [SerializeField] private float RotationSpeed;
@@ -14,6 +18,15 @@ public class BoomerangMotion : MonoBehaviour
         var centerOfPrefab = gameObject.transform.position;
         gameObject.transform.RotateAround(centerOfPrefab, YAxis, rotationSpeedInRadians*Time.deltaTime);
         
+        this.TimeElapsedInJourney += Time.deltaTime;
+        this.TimeElapsedInJourney = this.TimeElapsedInJourney % this.JourneyDuration;
+        var percentJourneyDone = this.TimeElapsedInJourney/this.JourneyDuration;
+        var radiansOfJourney = percentJourneyDone*2*Mathf.PI;
+        var forwardDistance = Mathf.Sin(radiansOfJourney)*MaxForwardDistance/2 + MaxForwardDistance/2;
+        var sidewaysDisplacement = Mathf.Sin(radiansOfJourney)*MaxSidewaysDisplacement;
+        centerOfPrefab.x = forwardDistance*this.transform.forward;
+        centerOfPrefab.z = sidewaysDisplacement;
+        gameObject.transform.position = centerOfPrefab;
     }
 }
 }
