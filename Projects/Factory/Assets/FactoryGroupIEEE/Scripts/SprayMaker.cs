71a5fe3faf608bfe97ff7cb8cbb8816499256281
Vincent Le <vvvle@ucdavis.edu>
2019-10-31 13:15:45 -0700

    Added spray maker/motion/bullet prefab

---
+++ Projects/Factory/Assets/FactoryGroupIEEE/Scripts/SprayMaker.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace IEEE
+{
+	public class SprayMaker : MonoBehaviour, IFactorySpell
+	{
+		[SerializeField]
+		private GameObject prefab;
+		[SerializeField]
+		private int number = 5;
+
+		void Start()
+		{
+		}
+
+		public GameObject Make()
+		{
+			GameObject newObject;
+			for (int i = 0; i < number - 1; i++)
+			{
+				newObject = Instantiate(this.prefab, this.gameObject.transform);
+			}
+			newObject = Instantiate(this.prefab, this.gameObject.transform);
+			return newObject;
+		}
+	}
+}
